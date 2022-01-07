2d0eb7588cf061dcc412d38044e580b20b5c0d29
anatolytsi <snipl33t@gmail.com>
2022-01-07 21:35:12 +0100

    added outside and inside temperature

--- src/components/Temperatures/Temperatures.js
+++ src/components/Temperatures/Temperatures.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Temperatures = ({temperatureOutside, temperatureInside}) => {
+const Temperatures = (
+    {
+        temperatureOutside,
+        temperatureInside,
+        temperatureHe1,
+        temperatureHe2,
+        temperatureHe3
+    }) => {
     return (
         <div>
             <div>
@@ -10,6 +17,15 @@ const Temperatures = ({temperatureOutside, temperatureInside}) => {
                 </p>
                 <p>
                     Температура в зале: {temperatureInside} ºC
+                </p>
+                <p>
+                    Температура теплообменника 1: {temperatureHe1} ºC
+                </p>
+                <p>
+                    Температура теплообменника 2: {temperatureHe2} ºC
+                </p>
+                <p>
+                    Температура теплообменника 3: {temperatureHe3} ºC
                 </p>
             </div>
         </div>
