33e7ae1897239f1ec347d2e1b9886a5c8414dbf8
anatolytsi <snipl33t@gmail.com>
2022-01-08 07:42:46 +0100

    added input step

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Input.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Input = ({value, setValue}) => {
+const Input = ({value, setValue, step}) => {
     return (
         <div className='def-number-input number-input' style={{display: 'inline-block'}}>
-            <button onClick={() => setValue(+value - 5)} className='minus'></button>
+            <button onClick={() => setValue(+value - step)} className='minus'></button>
             <input className='quantity' name='quantity' value={value}
                    onChange={(event) => setValue(event.target.value)}
                    type='number'/>
-            <button onClick={() => setValue(+value + 5)} className='plus'></button>
+            <button onClick={() => setValue(+value + step)} className='plus'></button>
         </div>
     );
 }
