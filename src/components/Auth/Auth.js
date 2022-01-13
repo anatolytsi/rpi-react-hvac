89a789eb0394e9d7162ae8bbc6a1c6f993bb982c
anatolytsi <snipl33t@gmail.com>
2022-01-14 00:33:29 +0100

    fixed templating

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -22,22 +22,20 @@ class LoginForm extends React.Component {
         return (
             <div className='container-fluid h-100'>
                 <div className='h-100 justify-content-center align-items-center d-flex'>
-                    <form className='col-2' onSubmit={(event => this.submitHandler(event))}>
+                    <form className='col-10' onSubmit={(event => this.submitHandler(event))}>
                         <div className='form-group'>
-                            <div className='form-group'>
-                                <label htmlFor='Login'>Логин</label>
-                                <input type='text' placeholder='Логин' id='Login' name='username'
-                                       className='form-control'
-                                       onChange={(event) => this.changeHandler(event)}/>
-                            </div>
-                            <div className='form-group pt-2 pb-2'>
-                                <label htmlFor='Password'>Пароль</label>
-                                <input type='password' placeholder='Пароль' id='Password' name='password'
-                                       className='form-control'
-                                       onChange={(event) => this.changeHandler(event)}/>
-                            </div>
-                            <button type='submit' className='btn btn-primary'>Войти</button>
+                            <label htmlFor='Login'>Логин</label>
+                            <input type='text' placeholder='Логин' id='Login' name='username'
+                                   className='form-control'
+                                   onChange={(event) => this.changeHandler(event)}/>
                         </div>
+                        <div className='form-group'>
+                            <label htmlFor='Password'>Пароль</label>
+                            <input type='password' placeholder='Пароль' id='Password' name='password'
+                                   className='form-control'
+                                   onChange={(event) => this.changeHandler(event)}/>
+                        </div>
+                        <button type='submit' className='btn btn-primary'>Войти</button>
                     </form>
                 </div>
             </div>
